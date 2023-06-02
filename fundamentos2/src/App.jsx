bbbf4b04a15669929746bdc99b00f6959c6c7d00
Felipe <felipe.edfreire@gmail.com>
2023-06-02 14:21:27 -0300

    state lift e prop children

--- fundamentos2/src/App.jsx
+++ fundamentos2/src/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import "./App.css";
 import Hand from "./assets/hand.jpg";
 import CarDetails from "./components/CarDetails";
 import ConditionalRender from "./components/ConditionalRender";
+import Container from "./components/Container";
+import ExecuteFunction from "./components/ExecuteFunction";
 import ListRender from "./components/ListRender";
 import { ManageData } from "./components/ManageData";
 import ShowUserName from "./components/ShowUserName";
+import Message from "./components/Message";
+import ChangeMessageState from "./components/ChangeMessageState";
 
 function App() {
+  const cars = [
+    { brand: "Fiat", km: 10000, color: "Vermelho" },
+    { brand: "VW", km: 5000, color: "Azul" },
+  ];
+
+  function showMessage() {
+    console.log("Evento do componente Pai");
+  }
+
+  const [message, setMessage] = useState("");
+
+  const handleMessage = (msg) => {
+    setMessage(msg);
+  };
+
   return (
     <div className="App">
       <div>
@@ -18,11 +38,30 @@ function App() {
       <div>
         <img src={Hand} height={500} />
       </div>
+      {/*Hooks*/}
       <ManageData />
       <ListRender />
       <ConditionalRender />
-      <ShowUserName name="Felps"/>
-      <CarDetails brand='Fiat' km={10000} color='Vermelho'/>
+
+      {/*Props*/}
+      <ShowUserName name="Felps" />
+
+      {/*Renderizando componentes com base em uma lista de objetos*/}
+      {cars.map((car, i) => (
+        <CarDetails key={i} brand={car.brand} km={car.km} color={car.color} />
+      ))}
+
+      {/*Children Props*/}
+      <Container>
+        <p>Children prop</p>
+      </Container>
+
+      {/*Funções como props*/}
+      <ExecuteFunction myFunction={showMessage} />
+
+      {/*State lift*/}
+      <Message msg={message} />
+      <ChangeMessageState handleMessage={handleMessage} />
     </div>
   );
 }
