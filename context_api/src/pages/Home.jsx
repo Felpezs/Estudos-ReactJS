6bb45e01d07b167713ca7242cb77cb7272d4bd81
Felipe <felipe.edfreire@gmail.com>
2023-06-07 14:03:06 -0300

    uso da contextAPI

---
+++ context_api/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+// import { useContext } from "react";
+// import { CounterContext } from "../context/CounterContext";
+import ChangeCounter from "../components/ChangeCounter";
+import { useCounterContext } from "../hooks/useCounterContex";
+
+const Home = () => {
+  // const { counter } = useContext(CounterContext);
+  const {counter} = useCounterContext()
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Valor do contador: {counter}</p>
+      <ChangeCounter />
+    </div>
+  );
+};
+
+export default Home;
