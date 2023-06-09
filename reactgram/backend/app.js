bb4c293b3a4927bd0f3e727bae9d8fc5b1cb6234
Felipe <felipe.edfreire@gmail.com>
2023-06-09 16:23:10 -0300

    configurações de projeto

--- reactgram/backend/app.js
+++ reactgram/backend/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -7,8 +7,21 @@ const port = process.env.PORT;
 
 const app = express();
 
+//Config form data and JSON response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Solve CORS
+app.use(cors({ credentials: true, origin: "http:localhost:3000" }));
+
+//Upload directory
+app.use("uploads", express.static(path.join(__dirname, "/uploads")));
+
+require("./config/db.js");
+
+const router = require("./routes/Router.js");
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`App rodando na porta: ${port}`);
