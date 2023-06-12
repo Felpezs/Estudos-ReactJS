3b3c9d5d4c43c417eacb759e79d46a8a0209b25a
Felipe <felipe.edfreire@gmail.com>
2023-06-12 14:13:43 -0300

    Validações e criação de dados no db

--- reactgram/backend/controller/UserController.js
+++ reactgram/backend/controller/UserController.js
@@ -7,10 +7,38 @@ const generateToken = (id) => {
   return jwt.sign({ id }, jwtSecret, { expiresIn: "7d" });
 };
 
-const register = async(req, res) => {
-    res.send("Registro")
-}
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: ["Email já cadastrado"] });
+    return;
+  }
+
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Um erro ocorreu, tente novamente mais tarde"] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
+};
 
 module.exports = {
-    register,
-}
+  register,
+};
