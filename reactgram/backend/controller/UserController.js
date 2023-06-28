1fdeafef685fa9bcdb7f5a2fd70020a12f92be84
Felipe Freire <felipe.edfreire@gmail.com>
2023-06-27 22:13:36 -0300

    feat: atualizacao de user

--- reactgram/backend/controller/UserController.js
+++ reactgram/backend/controller/UserController.js
@@ -1,6 +1,7 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const jwtSecret = process.env.JWT_SECRET;
 
 const generateToken = (id) => {
@@ -66,8 +67,64 @@ const getCurrentUser = async (req, res) => {
   res.status(200).json(user);
 };
 
+const update = async (req, res) => {
+  const { name, password, bio } = req.body;
+  let profileImage = null;
+
+  if (req.file) {
+    profileImage = req.file.filename;
+  }
+
+  const reqUser = req.user;
+
+  const user = await User.findById(reqUser._id).select("-password");
+
+  if (name) {
+    user.name = name;
+  }
+
+  if (password) {
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    user.password = passwordHash;
+  }
+
+  if (profileImage) {
+    user.profileImage = profileImage;
+  }
+
+  if (bio) {
+    user.bio = bio;
+  }
+
+  await user.save();
+
+  res.status(200).json(user);
+};
+
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id).select("-password");
+
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado."] });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+};
+
 module.exports = {
   register,
   login,
   getCurrentUser,
+  update,
+  getUserById,
 };
