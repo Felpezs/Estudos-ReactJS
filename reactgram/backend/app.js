cebb1f45db8098eb9e2393670071da02e98e6ecf
Felipe Freire <felipe.edfreire@gmail.com>
2023-07-01 16:38:01 -0300

    fix: cors

--- reactgram/backend/app.js
+++ reactgram/backend/app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const morgan = require("morgan")
+const morgan = require("morgan");
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -9,14 +9,19 @@ const port = process.env.PORT;
 const app = express();
 
 //Morgan config
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 //Config form data and JSON response
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Solve CORS
-app.use(cors({ credentials: true, origin: "http:localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: `http://localhost:${process.env.FRONT_END_PORT}`,
+  })
+);
 
 //Upload directory
 app.use("uploads", express.static(path.join(__dirname, "/uploads")));
